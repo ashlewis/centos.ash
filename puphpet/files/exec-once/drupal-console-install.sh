c481e3e49da4526532facb4912a63d9cd0839d2b
Ash Lewis <ash@s8080.com>
2016-06-15 10:00:20 +0100

    d8 updates

---
+++ puphpet/files/exec-once/drupal-console-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+echo 'Installing drupal-console'
+composer global require drupal/console:@stable
--- puphpet/files/exec-once/drush-commands-install.sh
+++ puphpet/files/exec-once/drush-commands-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #
-drush -y dl registry_rebuild
-drush -y dl site_audit
+echo 'Installing drush packages'
+# d8 support?
+#drush @none dl -y registry_rebuild
+cd ~/.drush && composer require drupal/site_audit:dev-8.x-3.x
