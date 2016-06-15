b7ba36596736e6def373e357ce4a7c4190769b46
Ash Lewis <ash@s8080.com>
2016-06-15 09:57:46 +0100

    ensure file is downloaed outside of repo

--- puphpet/files/exec-once/wkhtmltopdf-install.sh
+++ puphpet/files/exec-once/wkhtmltopdf-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #
+cd ~
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm && yum localinstall -y wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
